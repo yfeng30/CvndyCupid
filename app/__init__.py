f46a8d216ec1209ebb6096efba6fec82955e0595
Yuki Feng <yuki.2102005@gmail.com>
2023-06-05 13:32:44 -0400

    flask

---
+++ app/__init__.py
@@ -0,0 +1,69 @@
+# coding=utf8
+
+from flask import Flask, render_template, session, request, redirect
+import sqlite3
+import os #?
+from db_handle import *
+
+
+app = Flask(__name__)
+app.app_context().push()
+
+#for later
+app.secret_key = os.urandom(32)
+
+
+
+#i forgot difference between get/post!
+#code not shamelessly repurposed from p3
+@app.route('/', methods=['GET'])
+def login():
+    if 'username' in session: #when cookie work
+    #if(False):
+        return redirect('/home/pl')
+    return render_template('login.html') #names subject to change
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  return render_template('register.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def make_account():
+  if check_user(request.form.get('username')): #NEED a method to verify if a username is taken
+  #if(False):
+    return render_template('register.html', status='Username is in use!')
+
+  #new entry
+  print(request.form.get('password'))
+  print(request.form.get('password_confirm'))
+  if (request.form.get('password') != request.form.get('password_confirm')):
+    return render_template('register.html', status='The passwords typed are not the same!')
+  print("register " + request.form.get('username') + " " + request.form.get('password'))
+  create_user(request.form.get('username'), request.form.get('password')) #NEED method to create an entry in the table
+  session['username'] = request.form['username']
+  print(session['username'])
+  return redirect('/home/pl') #/home or /
+  
+#this method actually verifies whether or not the login works
+@app.route('/auth', methods=['GET', 'POST'])
+def authenticate():
+  #print(check_pass(request.form.get('username'), request.form.get('password')))
+  #print("login " + request.form.get('username') + " " + request.form.get('password'))
+  if not (check_user(request.form.get('username'))):
+    return render_template('login.html', status="User doesn't exist!")
+  if not (check_pass(request.form.get('username'), request.form.get('password'))): #NEED method to take in a username and password and return if that entry exists
+    return render_template('login.html', status='Incorrect password!')
+  session['username'] = request.form['username']
+  return redirect('/home/pl')
+
+#forgot to actually allow a logout (I think it was in the site map)
+@app.route('/logout')
+def logout():
+  session.pop('username')
+  #"popped user: " + session.pop('username'))
+  return redirect('/')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
