696e2e588dc916f150298c1f9f9df4079c9858ac
aymanhabib <109638308+aymanhabib@users.noreply.github.com>
2023-06-06 22:07:53 -0400

    Add files via upload

---
+++ app/static/js/script.js
@@ -0,0 +1,10 @@
+function swipeLeft() {
+    console.log("Swiped left");
+    // Add your logic for swiping left here
+  }
+  
+  function swipeRight() {
+    console.log("Swiped right");
+    // Add your logic for swiping right here
+  }
+  
